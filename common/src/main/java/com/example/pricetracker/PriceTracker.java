2a4a50736a64c60e8e1070114d0572725264eb03
Om Gupta <ogupta4242@gmail.com>
2024-02-19 15:18:47 -0800

    JSoup still has some issues with me
    Look in proguard-rules.pro for the fix that's supposed to work

--- common/src/main/java/com/example/pricetracker/PriceTracker.java
+++ common/src/main/java/com/example/pricetracker/PriceTracker.java
@@ -15,16 +15,16 @@ public class PriceTracker extends Lifecycle {
     @Override
     public void runApp() {
         Form hi = new Form("Hi World", BoxLayout.y());
-        Button helloButton = new Button("Hello World");
+        Button helloButton = new Button("Show Product Info");
         hi.add(helloButton);
         helloButton.addActionListener(e -> hello());
-        hi.getToolbar().addMaterialCommandToSideMenu("Hello Command",
-        FontImage.MATERIAL_CHECK, 4, e -> hello());
+        hi.getToolbar().addMaterialCommandToSideMenu("Hello Command", FontImage.MATERIAL_CHECK, 4, e -> hello());
         hi.show();
     }
-
+    
     private void hello() {
-        Dialog.show("Hello Codename One", "Welcome to Codename One", "OK", null);
+        Product p = new Product("https://www.amazon.com/AmazonBasics-Matte-Keyboard-QWERTY-Layout/dp/B07WJ5D3H4/ref=sr_1_5?crid=1HNR5G8ZU9BQV&dib=eyJ2IjoiMSJ9.iQx-lJra_ejVJqdnNACdKZng8aRk-GUZrm3hrHbhIIxBSHhUS0KbTzuPYO3rSMlwd45bBxfIui-lZeD2-DRI6AC162oc8ZJtiWiXQCw038SXeOgw5jyxyZisstdrZlTUrXhH2WQla9Q03zcpxT-EiCVy80fxSLqBvNCkL70ff_yYOHwWa8grBIhdT2jWeDgK1dN3FAsbGS27HJlo1twGOFqO3WKtm6PUw5fWgcXydKg.bu6kFOcXK9l9PgBlnM9sYIj2kVoZbPw-x9DDMekP2JQ&dib_tag=se&keywords=keyboard&qid=1708382782&sprefix=keyboar%2Caps%2C148&sr=8-5&th=1");
+        Dialog.show("Product Info", p.toString(), "OK", null);
     }
-
+    
 }
